9a16ab32dd1a9ea5c992bf313b70d7651a6c26e6
Oein <oein0219@gmail.com>
2022-09-11 12:02:30 +0900

    Docs Init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js',
-  unstable_staticImage: true,
+const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
+module.exports = withNextra({
+  i18n: {
+    locales: ['en', 'kr'],
+    defaultLocale: 'kr',
+  },
 })
-module.exports = withNextra()
